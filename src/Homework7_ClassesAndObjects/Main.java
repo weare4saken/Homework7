d1f88aef9ce567fed373d396c2a3b6bcca19b438
weare4saken <kondrashka91@gmail.com>
2022-12-12 13:18:57 +0700

    Second commit

---
+++ src/Homework7_ClassesAndObjects/Main.java
@@ -0,0 +1,11 @@
+package Homework7_ClassesAndObjects;
+
+public class Main {
+    public static void main (String[] args) {
+        Author julianBarns = new Author("Julian", "Barns");
+        Author vicktorPelevin = new Author("Виктор", "Пелевин");
+        Book theOnlyStory = new Book("The Only Story", julianBarns, 2018);
+        Book t = new Book ("t", vicktorPelevin, 2009);
+        t.setYearOfPublication(2011);
+    }
+}
